9962bf3230e49a7b54e0ce66f091a674ff88c2b4
krisinho <krinya125@gmail.com>
2023-03-03 16:11:00 +0100

    add PersonDao.kt and JingleDatabase.kt, TypeConverters.kt classes refactor Person.kt (use @Entity on it to connect it with Room database)
    fix androidKapt gradle build failure (using room 2.3.0 instead of 2.1.0)

---
+++ app/src/main/java/com/example/demodatingapp/db/dao/PersonDao.kt
@@ -0,0 +1,22 @@
+package com.example.demodatingapp.db.dao
+
+import androidx.lifecycle.LiveData
+import androidx.room.*
+import com.example.demodatingapp.vo.Person
+
+@Dao
+interface PersonDao {
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(person: Person)
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insert(persons: List<Person>)
+
+    @Query("SELECT * FROM person")
+    fun findAll(): LiveData<List<Person>>
+
+    @Query("SELECT * FROM person WHERE id = :id")
+    fun findById(id: Int): LiveData<Person>
+
+}
