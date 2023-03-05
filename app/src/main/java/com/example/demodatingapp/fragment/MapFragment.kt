3a4c10eb85b26ff3fb3d6b395a1c1e29e6008b1b
krisinho <krinya125@gmail.com>
2023-03-05 18:26:38 +0100

    add list view on map fragment add back navigation to list view

--- app/src/main/java/com/example/demodatingapp/fragment/MapFragment.kt
+++ app/src/main/java/com/example/demodatingapp/fragment/MapFragment.kt
@@ -1,16 +1,26 @@
 package com.example.demodatingapp.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import android.view.*
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.onNavDestinationSelected
 import com.example.demodatingapp.R
 
 class MapFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val binding = DataBindingUtil.inflate<com.example.demodatingapp.databinding.FragmentMapBinding>(inflater, R.layout.fragment_map, container, false)
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.map_menu, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(findNavController()) || super.onOptionsItemSelected(item)
     }
 }
